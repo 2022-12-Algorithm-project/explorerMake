bd80ea878d3c1ce28b9dfa22abe8c6ba254fff6c
qlido <jason7530@naver.com>
2022-12-08 09:30:32 +0900

    코드 정리

--- main.cpp
+++ main.cpp
@@ -3,24 +3,25 @@
 #include <utility>
 #include <Windows.h>
 #include <iomanip>
+
 #define MaxDirCount 20
 #define MaxFileCount 20
 
-typedef struct File{
+typedef struct File {
     std::string name;
-}File;
+} File;
 
-typedef struct Directory{
+typedef struct Directory {
     std::string name;
-    int Dcount;
-    int Fcount;
-    struct Directory* LastDir;
-    struct Directory* SubDir[MaxDirCount]{};
-    struct File* SubFile[MaxFileCount]{};
-}Directory;
+    int Dcount{};
+    int Fcount{};
+    struct Directory *LastDir{};
+    struct Directory *SubDir[MaxDirCount]{};
+    struct File *SubFile[MaxFileCount]{};
+} Directory;
 
-Directory* RootDir(std::string Username){
-    auto* root = new Directory;
+Directory *RootDir(std::string Username) {
+    auto *root = new Directory;
     root->name = std::move(Username);
     root->Dcount = 0;
     root->Fcount = 0;
@@ -28,103 +29,102 @@ Directory* RootDir(std::string Username){
     return root;
 }
 
-int search(Directory* root,std::string target){
-    Directory* temp = root;
-    try {
+int search(Directory *root, std::string target) {
+    Directory *temp = root;
         for (int i = 0; i < temp->Dcount; i++) {
             if (temp->SubDir[i]->name == target)
                 return i;
         }
         return -1;
-    }catch (std::exception e){
-        std::cout << "에러가 났어요" << std::endl;
-    }
+
 }
 
-void macro(Directory* Root,std::string username) {
+void macro(Directory *Root, std::string username) {
     system("color 02");
     std::cout << username;
     system("color");
-    if(Root->name == username) {
+    if (Root->name == username) {
 
         std::cout << ":~$ >> ";
-    }
-    else{
+    } else {
 
-        std::cout << ":" << Root->name << " $ >> " ;
+        std::cout << ":" << Root->name << " $ >> ";
     }
 
 }
 
-Directory* ChangeDir(std::string CL,Directory* root){
+Directory *ChangeDir(std::string CL, Directory *root) {
     try {
         Directory *temp = root;
+        if (CL == "..") {
+            if (!(temp->LastDir)) {
+                std::cout << "처음 디렉토리 입니다" << std::endl;
+                return temp;
+            } else {
+                temp = temp->LastDir;
+                return temp;
+            }
+        }
         int index = search(temp, std::move(CL));
         if (index > -1) {
-            //root = root->SubDir[/*인덱스값*/ 0];
             temp = temp->SubDir[index];
             return temp;
         } else {
             std::cout << "없는 디렉토리 입니다" << std::endl;
             return temp;
         }
-    }catch(std::exception e){
+    } catch (std::exception e) {
         std::cout << "에러가 발생했습니다 명령어를 점검해주십시오" << std::endl;
     }
 }
 
 
-void MakeDir(Directory* root,std::string ND){
-    Directory* temp = root;
-
-    int i=0;
+void MakeDir(Directory *root, std::string ND) {
+    Directory *temp = root;
+    int i = 0;
     try {
         while (i < temp->Dcount) {
-
             if (temp->SubDir[i]->name == ND)
                 throw ND;
             i++;
         }
-        Directory* NewDir = RootDir(ND);
+        Directory *NewDir = RootDir(ND);
         NewDir->LastDir = temp;
         temp->SubDir[i] = NewDir;
         temp->Dcount++;
-    }catch (std::string e){
+    } catch (std::string e) {
         std::cout << e << "는 이미 있는 폴더명 입니다" << std::endl;
     }
-
 }
-void MakeFile(Directory* root,std::string NF){
-    Directory* temp = root;
 
-    int i=0;
+void MakeFile(Directory *root, std::string NF) {
+    Directory *temp = root;
+    int i = 0;
     try {
         while (i < temp->Dcount) {
             if (temp->SubFile[i]->name == NF)
                 throw NF;
             i++;
         }
-        File* NewFile = new File;
+        File *NewFile = new File;
         NewFile->name = NF;
         temp->SubFile[i] = NewFile;
         temp->Fcount++;
-    }catch (std::string e){
+    } catch (std::string e) {
         std::cout << e << "는 이미 있는 파일명 입니다" << std::endl;
     }
-
 }
 
-void List(Directory* root){
-    Directory* temp = root;
-    for(int i=0; i<temp->Dcount;i++){
+void List(Directory *root) {
+    Directory *temp = root;
+    for (int i = 0; i < temp->Dcount; i++) {
         std::cout << temp->SubDir[i]->name << "/ ";
     }
-    for(int j=0; j<temp->Fcount;j++){
+    for (int j = 0; j < temp->Fcount; j++) {
         std::cout << temp->SubFile[j]->name << " ";
     }
     std::cout << std::endl;
 }
-
 
 
 int main() {
@@ -133,39 +133,33 @@ int main() {
     std::string Username;
     std::cin >> Username;
     system("cls");
-    Directory* root = RootDir(Username);
-    Directory* NL = root;
+    Directory *root = RootDir(Username);
+    Directory *NL = root;
     std::string cmd;
     std::cin.ignore();
-    while(cmd != "exit") {
-        try{
-        macro(NL,Username);
-        getline(std::cin,cmd,'\n');
-        //Command(cmd,NL);
-        if(cmd.rfind("cd", 0) == 0){
-            std::string temp = cmd.substr(3);
-            NL = ChangeDir(temp,NL);
+    while (cmd != "exit") {
+        try {
+            macro(NL, Username);
+            getline(std::cin, cmd, '\n');
+            //Command(cmd,NL);
+            if (cmd.rfind("cd", 0) == 0) {
+                std::string temp = cmd.substr(3);
+                NL = ChangeDir(temp, NL);
+            } else if (cmd.rfind("mkdir", 0) == 0) {
+                std::string temp = cmd.substr(6);
+                MakeDir(NL, temp);
+            } else if (cmd.rfind("touch", 0) == 0) {
+                std::string temp = cmd.substr(6);
+                MakeFile(NL, temp);
+            } else if (cmd.rfind("ls", 0) == 0) {
+                List(NL);
+            } else {
+                std::cout << "가상 파일 탐색기 입니다\n명령어로는 ls,cd,mkdir,exit이 있습니다" << std::endl;
+            }
+        } catch (std::exception e) {
+            std::cout << "에러가 발생했습니다 명령어를 점검해주십시오" << std::endl;
         }
-        else if(cmd.rfind("mkdir", 0) == 0){
-            std::string temp = cmd.substr(6);
-            MakeDir(NL,temp);
-        }
-        else if(cmd.rfind("touch", 0) == 0){
-            std::string temp = cmd.substr(6);
-            MakeFile(NL,temp);
-        }
-        else if(cmd.rfind("ls", 0) == 0){
-            List(NL);
-        }
-
-        else{
-            std::cout << "가상 파일 탐색기 입니다\n명령어로는 ls,cd,mkdir,exit이 있습니다" << std::endl;
-        }
-    }catch(std::exception e){
-        std::cout << "에러가 발생했습니다 명령어를 점검해주십시오" << std::endl;
     }
-    }
-
 
 
     return 0;
